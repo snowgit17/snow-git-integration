2980c4650f9ee21f44a4c090d54161754ffa3d60
snowgit17 <aniketraut0722@gmail.com>
2025-03-25 16:59:14 +0530

    Update VWH.sql

--- VWH.sql
+++ VWH.sql
@@ -2,6 +2,17 @@ use role accountadmin;
 use database aniket;
 use schema mart;
 
+CREATE or replace TABLE aniket.mart.employee (
+dept_id int,
+id int,
+firstname varchar (20),
+middlename varchar (20),
+lastname varchar (20),
+age int,
+salary int, 
+location varchar (20),
+join_date varchar(30)
+);
 
 CREATE or replace WAREHOUSE git_vwh
   WAREHOUSE_SIZE = 'XSMALL'
